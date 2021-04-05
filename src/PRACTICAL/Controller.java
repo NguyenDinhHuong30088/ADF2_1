7e094830135a9e7e6658a11edb8a6692932c4bc4
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2021-04-05 11:42:38 +0700

    UP

--- src/PRACTICAL/Controller.java
+++ src/PRACTICAL/Controller.java
@@ -1,4 +1,43 @@
 package PRACTICAL;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public TableView<Contact> tbView;
+    public TableColumn<Contact,String> tbName;
+    public TableColumn<Contact,String> tbPhone;
+    ObservableList<Contact> ct = FXCollections.observableArrayList();
+    ObservableList<Contact> ctSearch = FXCollections.observableArrayList();
+    public TextField Name;
+    public TextField Phone;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tbName.setCellValueFactory(new PropertyValueFactory<Contact,String>("Name"));
+        tbPhone.setCellValueFactory(new PropertyValueFactory<Contact,String>("Phone"));
+    }
+    public void add(){
+        String n = Name.getText();
+        String p = Phone.getText();
+        if(!n.isEmpty() && !p.isEmpty()){
+            Contact c = new Contact(n,p);
+            ct.add(c);
+            tbView.setItems(ct);
+        }
+    }
+    public void search(){
+
+    }
+    public void exit(){
+
+    }
 }
--- src/PRACTICAL/Main.java
+++ src/PRACTICAL/Main.java
@@ -1,6 +1,24 @@
 package PRACTICAL;
 
-public class Main
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-{
+public class Main extends Application {
+    public static Stage menuStage;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("PRACTICLE/Address.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1283, 813));
+        primaryStage.show();
+        menuStage = primaryStage;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
